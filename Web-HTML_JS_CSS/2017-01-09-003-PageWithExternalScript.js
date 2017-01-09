86deed789cac559f3f0466e817eeaf78f5ac6a9e
hiyangdong <hiyangdong@hotmail.com>
2017-01-09 19:24:51 +0800

    + Page With External Script（html+js)

---
+++ Web-HTML_JS_CSS/2017-01-09-003-PageWithExternalScript.js
@@ -0,0 +1,10 @@
+﻿/*
+  Author：hiyangdong
+  Time  ：2017-01-09 19:22:43
+*/
+function hello(){
+	document.getElementById("demo").innerHTML="After Click Botton";
+}
+function change(){
+	document.getElementById("demo").innerHTML="Change by click string";
+}
