1200f16957c3cc08c4f9229fab28dc1dcb8fdc87
Dong YANG <Dong YANG>
2017-02-06 20:07:39 +0800

    添加机器学习相关内容

---
+++ Tensorflow-DeepLearning/TensorflowProject/tf_minst_softmax.py
@@ -0,0 +1,30 @@
+﻿import tensorflow.examples.tutorials.mnist.input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+import tensorflow as tf
+x = tf.placeholder(tf.float32, [None, 784])
+
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+y_ = tf.placeholder("float", [None,10])
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+
+
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+init = tf.initialize_all_variables()
+# 现在我们可以在一个Session里面启动我们的模型，并且初始化变量：
+
+sess = tf.Session()
+sess.run(init)
+# 然后开始训练模型，这里我们让模型循环训练1000次！
+
+for i in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+
+
+
+
---
+++ Web-PageTemplate/html5-css3-fluid-menu-with-icon/js/index.js
@@ -0,0 +1,9 @@
+/*
+Fluid menu with transparent icons.
+----------------------------------------------
+Designed by Vincent Perhirin.
+http://www.behance.net/vincentperhirin
+----------------------------------------------
+https://twitter.com/One_div
+http://one-div.com <-- CSS3 single element database
+*/
