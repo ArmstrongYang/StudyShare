bc715add34f5b49a337e36fc539f463fdfc5a66d
Dong YANG <Dong YANG>
2017-03-04 00:04:42 +0800

    更新mongodb的cmd说明

--- Python-Mangodb/mongo-toollists.py
+++ Python-Mangodb/mongo-toollists.py
@@ -1,7 +1,9 @@
 import os
 import pymongo
 
-# 需要通过cmd打开数据库服务器：mongod --dbpath=E:/onedrive/databases --port 2222
+# 需要通过cmd打开数据库服务器：mongod --dbpath=E:/onedrive/databases/db --port 2222 --logpath=E:/onedrive/databases/log/mongodb.log 
+# 如果需要后台运行，添加--fork,即
+# mongod --dbpath=E:/onedrive/databases --port 2222 --fork
 client = pymongo.MongoClient('localhost', 2222)
 
 
